b086de5fff1a4e96897d6ec8f311df1e21475e66
lizitao <634376133@qq.com>
2022-11-30 16:16:52 +0800

    Script Submission

--- AKOCommonToolsKit/Classes/AKOCommonToolsKit.h
+++ AKOCommonToolsKit/Classes/AKOCommonToolsKit.h
@@ -8,7 +8,7 @@
 #ifndef AKOCommonToolsKit_h
 #define AKOCommonToolsKit_h
 
-#import <AKOCommonToolsKit/MDCommonMacroDefine.h>
+#import <AKOCommonToolsKit/AKOCommonMacroDefine.h>
 #import <AKOCommonToolsKit/NSArray+functional.h>
 #import <AKOCommonToolsKit/NSString+Ext.h>
 #import <AKOCommonToolsKit/NSURL+Ext.h>
